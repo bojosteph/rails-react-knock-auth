74bfb0e5233f62548b37ac716a08c80fac707607
Roberto Samia <robertosamia@Robertos-Mini.attlocal.net>
2019-08-20 18:52:38 -0400

    added react package yarn install

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles
   def index
-    @articles = Article.all
+    @articles = Article.all.order(created_at: :desc)
 
     render json: @articles
   end
